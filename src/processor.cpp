a8b9275576fccfb6af7c8dd0167f97e9d361fc08
Ibrahim Khanani <ibrahimkhanani@yahoo.com>
2021-01-01 16:39:59 +0000

    Completed Project

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,13 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <iostream>
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+    int idle = LinuxParser::IdleJiffies();
+    int active = LinuxParser::ActiveJiffies();
+    //std::cout << active << "\n";
+    //std::cout << (float)active/(float)(idle + active) << "\n";
+    return (float)active/(float)(idle + active);
+
+    }
